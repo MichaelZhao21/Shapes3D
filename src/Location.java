54041185faa67177851488aad530f5aa013e289f
Michael Zhao <37679458+MichaelZhao21@users.noreply.github.com>
2022-08-13 11:26:18 -0500

    Initial Commit

---
+++ src/Location.java
@@ -0,0 +1,17 @@
+public class Location {
+    public double x;
+    public double y;
+    public double z;
+
+    public Location() {
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+
+    public Location(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
---
+++ src/Shape3D.java
@@ -0,0 +1,45 @@
+public abstract class Shape3D {
+    protected String name;
+    protected String color;
+    protected Location loc;
+
+    public Shape3D() {
+        name = "";
+        color = "";
+        loc = new Location();
+    }
+
+    public Shape3D(String name, String color, Location loc) {
+        this.name = name;
+        this.color = color;
+        this.loc = loc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Location getLoc() {
+        return loc;
+    }
+
+    public void setLoc(Location loc) {
+        this.loc = loc;
+    }
+
+    public abstract double calculateVolume();
+
+    public abstract double calculateSurfaceArea();
+}
